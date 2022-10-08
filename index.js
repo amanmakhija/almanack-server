974f241a78495b68d1f6a8f77981140f1f268352
Aman Makhija <amanmakhija2205@gmail.com>
2022-10-08 10:52:23 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,166 +1,142 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const PORT = 8000;
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const uri = "mongodb+srv://admin-aman:<admin-aman2003>@almanackdb.daxch4x.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-mongoose.connect("mongodb://localhost:27017/eventDB");
+client.connect(err => {
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-//User Schema
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-});
-const User = mongoose.model("User", userSchema);
+    const User = client.db("almanackDB").collection("users");
+    const Event = client.db("almanackDB").collection("events");
+    // perform actions on the collection object
 
-//Event Schema
-const eventSchema = new mongoose.Schema({
-    name: String,
-    date: Date,
-    timeFrom: Number,
-    timeTo: Number,
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true
-    },
-    image: {
-        public_id: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        }
-    }
-});
-const Event = mongoose.model("Event", eventSchema);
+    //Middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.get('/', (req, res) => {
+        res.send('Server is Running Properly');
+    });
 
 
-app.get('/', (req, res) => {
-    res.send('Server is Running Properly');
-});
+    //User Routes
+    app.post('/register', async (req, res) => {
+        const { name, email, password } = req.body;
 
+        const savedUser = await User.findOne({ email });
 
-//User Routes
-app.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
+        if (savedUser) {
+            res.status(400).json({
+                success: false,
+                message: 'User already existed with the given Email ID'
+            });
+        } else {
+            const user = await User.create({
+                name,
+                email,
+                password
+            });
 
-    const savedUser = await User.findOne({ email });
-
-    if (savedUser) {
-        res.status(400).json({
-            success: false,
-            message: 'User already existed with the given Email ID'
-        });
-    } else {
-        const user = await User.create({
-            name,
-            email,
-            password
-        });
-
-        res.status(200).json({
-            success: true,
-            user
-        });
-    }
-});
-
-app.get('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-
-    if (!user) {
-        res.status(400).json({
-            success: false,
-            message: 'Email ID or Password Incorrect'
-        });
-    } else {
-        if (user.password === password) {
             res.status(200).json({
                 success: true,
                 user
             });
-        } else {
+        }
+    });
+
+    app.post('/login', async (req, res) => {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
             res.status(400).json({
                 success: false,
                 message: 'Email ID or Password Incorrect'
             });
+        } else {
+            if (user.password === password) {
+                res.status(200).json({
+                    success: true,
+                    user
+                });
+            } else {
+                res.status(400).json({
+                    success: false,
+                    message: 'Email ID or Password Incorrect'
+                });
+            }
         }
-    }
-});
+    });
 
-app.get('/profile', async (req, res) => {
-    const { userID } = req.body;
+    app.get('/profile', async (req, res) => {
+        const { userID } = req.body;
 
-    const user = await User.findOne({ userID });
+        const user = await User.findOne({ userID });
 
-    if (!user) {
-        res.status(400).json({
-            success: false,
-            message: 'No Users Found'
+        if (!user) {
+            res.status(400).json({
+                success: false,
+                message: 'No Users Found'
+            });
+        } else {
+            const events = await Event.find({ user: userID });
+
+            res.status(200).json({
+                success: true,
+                user,
+                events
+            });
+        }
+    });
+
+
+    //Event Routes
+    app.post('/event/new', async (req, res) => {
+        const { name, date, timeFrom, timeTo, image } = req.body;
+
+        const event = await Event.create({
+            name,
+            date,
+            timeFrom,
+            timeTo,
+            user: req.body.currentUser,
+            image
         });
-    } else {
-        const events = await Event.find({ user: userID });
 
         res.status(200).json({
             success: true,
-            user,
+            event
+        });
+    });
+
+    app.get("/events", async (req, res) => {
+        const events = await Event.find({});
+
+        res.status(200).json({
+            success: true,
             events
         });
-    }
-});
-
-
-//Event Routes
-app.post('/event/new', async (req, res) => {
-    const { name, date, timeFrom, timeTo, image } = req.body;
-
-    const event = await Event.create({
-        name,
-        date,
-        timeFrom,
-        timeTo,
-        user: req.body.currentUser,
-        image
     });
 
-    res.status(200).json({
-        success: true,
-        event
+    app.get('/event/:eventID', async (req, res) => {
+        const eventID = req.params.eventID;
+
+        const event = await Event.findById(eventID);
+
+        res.status(200).json({
+            success: true,
+            event
+        });
     });
+
+    client.close();
 });
-
-app.get("/events", async (req, res) => {
-    const events = await Event.find({});
-
-    res.status(200).json({
-        success: true,
-        events
-    });
-});
-
-app.get('/event/:eventID', async (req, res) => {
-    const eventID = req.params.eventID;
-
-    const event = await Event.findById(eventID);
-
-    res.status(200).json({
-        success: true,
-        event
-    });
-});
-
 
 //Listening On PORT
 app.listen(PORT, (err) => {
